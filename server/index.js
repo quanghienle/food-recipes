350b3c2fc740f6fe9b9facfd564cc34a0f6fa875
Hien Le <quanghien.a1@gmail.com>
2021-08-07 11:43:34 -0400

    REC-11: connect to mysql

--- server/index.js
+++ server/index.js
@@ -6,6 +6,27 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host: '127.0.0.1',
+  user: 'root',
+  password: '****',
+  database: 'recipes'
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('Connected to MySQL Server!');
+});
+
+app.get("/recipes",(req,res) => {
+    connection.query('SELECT * from recipes LIMIT 10', (err, rows) => {
+        if(err) throw err;
+        res.json(rows);
+        connection.end();
+    });
+});
+
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
