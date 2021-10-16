aa5b291dd46759e3ca6b7a27314190fccad3824e
Mike Nguyen <minhmnguyen@cmail.carleton.ca>
2021-10-16 16:34:02 -0400

    Refractor App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,13 @@
+import NavBar from "./components/NavBar";
 import React from "react";
-import Box from "@mui/material/Box";
-import RecipeCard from "./components/RecipeCard";
+import RecipesPage from "./pages/RecipesPage";
+
 
 function App() {
-  const [recipes, setRecipes] = React.useState([]);
-
-  React.useEffect(() => {
-    fetch("/recipes")
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        setRecipes(data);
-      });
-  }, []);
-
   return (
     <div style={{ width: "100%" }}>
-      <Box
-        sx={{
-          display: "flex",
-          flexWrap: "wrap",
-          flexDirection: "row",
-          p: 1,
-          m: 1,
-          bgcolor: "background.paper",
-        }}
-      >
-        {recipes.map((recipe) => (
-          <Box sx={{ p: 1, flexGrow: 1 }} key={`recipe-preview-${recipe.id}`}>
-            <RecipeCard
-              title={recipe.name}
-              imagePath={"./images/recipe-placeholder.png"}
-              timestamp={recipe.submitted}
-              description={recipe.description}
-              steps={recipe.steps}
-            />
-          </Box>
-        ))}
-      </Box>
+        <NavBar></NavBar>
+        <RecipesPage/>
     </div>
   );
 }
