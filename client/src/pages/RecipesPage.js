bbec3840542cfc777ffae2497e34ed2ac536411e
Mike Nguyen <minhmnguyen@cmail.carleton.ca>
2021-10-16 16:32:13 -0400

    Create Recipes Page

---
+++ client/src/pages/RecipesPage.js
@@ -0,0 +1,43 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import RecipeCard from "../components/RecipeCard";
+
+
+export default function RecipesPage(){
+
+  const [recipes, setRecipes] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("/recipes")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setRecipes(data);
+      });
+  }, []);
+  
+    return (
+        <Box
+        sx={{
+          display: "flex",
+          flexWrap: "wrap",
+          flexDirection: "row",
+          p: 1,
+          m: 1,
+          bgcolor: "background.paper",
+        }}
+      >
+        {recipes.map((recipe) => (
+          <Box sx={{ p: 1, flexGrow: 1 }} key={`recipe-preview-${recipe.id}`}>
+            <RecipeCard
+              title={recipe.name}
+              imagePath={"./images/recipe-placeholder.png"}
+              timestamp={recipe.submitted}
+              description={recipe.description}
+              steps={recipe.steps}
+            />
+          </Box>
+        ))}
+      </Box>
+    )
+}
