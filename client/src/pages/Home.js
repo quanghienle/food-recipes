056479a585dea9970753bb733a324dcf4a654931
Mike Nguyen <minhmnguyen@cmail.carleton.ca>
2021-11-06 08:09:00 -0400

    added layout for Home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,38 @@
 import React from "react";
 import { Link, } from "react-router-dom";
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
 
+const Item = styled(Paper)(({ theme }) => ({
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'left',
+    color: 'black',
+    backgroundColor: '#8ed8e6'
+}));
 
 export default function Home(){
-  
     return (
-        <h2>
-            Welcome to <Link to="/recipes">Recipes</Link>
-        </h2>
-    )
+        <Box sx={{ flexGrow: 1}}>
+            <Grid container spacing={4} direction='column'>
+                <Grid item xs={12 }>
+                    <Item>
+                        to display background
+                    </Item>
+                </Grid>
+                <Grid item xs={12}>
+                    <Item>
+                        to display top rated recipes <Link to="/recipes">Recipes</Link>
+                    </Item>
+                </Grid>
+                <Grid item xs={12}>
+                    <Item>
+                        to display popular cusines <Link to="/cusines">Cusines</Link>
+                    </Item>
+                </Grid>
+            </Grid>
+        </Box>
+    );
 }
