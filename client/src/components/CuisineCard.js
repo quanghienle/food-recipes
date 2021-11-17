55f0a680eb72d4b779a553914d003956e25786c0
Mike Nguyen <minhmnguyen@cmail.carleton.ca>
2021-11-16 19:03:06 -0500

    added cuisine card component

---
+++ client/src/components/CuisineCard.js
@@ -0,0 +1,27 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import { Link } from "react-router-dom";
+import { style } from "@mui/system";
+
+export default function CuisineCard(props) {
+    const {cuisineName, imagePath}= props;
+    
+    return (
+        <Card sx={{ width: 345, height: "100%", m: "auto" }}>
+            <CardHeader
+                style={{ textTransform: "capitalize" }}
+                title={cuisineName }
+            />
+            <Link to={`/cuisines/${cuisineName}`}>
+                <CardMedia
+                component="img"
+                height="194"
+                image={imagePath}
+                alt="Cuisine Name"
+                />
+            </Link>
+        </Card>
+    );
+}
