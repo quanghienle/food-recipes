93dda15cc799b0b30be96ef56f45fc683b2600fd
Mike Nguyen <minhmnguyen@cmail.carleton.ca>
2021-11-17 07:19:47 -0500

    init CuisinePage

---
+++ client/src/pages/CuisinePage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import RecipeCard from "../components/RecipeCard";
+import { Link } from "react-router-dom";
+
+export default function CuisinesPage() {
+    
+
+
+
+}
