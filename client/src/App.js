9a5cd55d70a8295912750b96c3ebdd9bf3179701
Mike Nguyen <minhmnguyen@cmail.carleton.ca>
2021-12-07 15:33:28 -0500

    refractor routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import NavBar from "./components/NavBar";
 import React from "react";
 import RecipesPage from "./pages/RecipesPage";
 import RecipeDetail from "./pages/RecipeDetail";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 import Home from "./pages/Home";
 import {BrowserRouter, Redirect, Route, Switch, withRouter} from "react-router-dom";
 import { makeStyles } from "@mui/styles";
@@ -17,7 +19,7 @@ const useStyles = makeStyles({
 
 export default function App() {
   const classes = useStyles();
-  const redirectHome = () => <Redirect to="/home" />;
+  const redirectHome = () => <Redirect to="/signin" />;
 
   return (
     <div className={classes.root}>
@@ -26,6 +28,8 @@ export default function App() {
       <div className={classes.container}>
         <BrowserRouter>
           <Switch>
+            <Route exact path="/signin" component={withRouter(SignIn)} />
+            <Route exact path="/signup" component={withRouter(SignUp)} />
             <Route exact path="/" render={redirectHome} />
             <Route exact path="/home" component={withRouter(Home)} />
             <Route exact path="/recipes" component={withRouter(RecipesPage)} />
